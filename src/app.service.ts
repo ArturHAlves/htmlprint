2587295a92fc90d0575f1e83ac558b8efdb91cbf
artur.alves <artur.henrique@defensoria.mg.def.br>
2025-02-21 10:52:01 -0300

    fix/ajuste versao puppeteer

--- src/app.service.ts
+++ src/app.service.ts
@@ -4,7 +4,9 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   async generatePdf(html: string) {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    });
     const page = await browser.newPage();
 
     await page.setContent(html, { waitUntil: 'load' });
