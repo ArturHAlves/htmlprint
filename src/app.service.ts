a55548c27e03e18cc4f7da064f2c67cb7bec0743
artur.almeida <artur.henrique@defensoria.def.mg.br>
2025-02-24 10:51:18 -0300

    feat/adicionando nova alternativa de lib

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,19 +1,25 @@
-import * as puppeteer from 'puppeteer';
 import { Injectable } from '@nestjs/common';
+import { chromium } from 'playwright';
 
 @Injectable()
 export class AppService {
   async generatePdf(html: string) {
-    const browser = await puppeteer.launch({
-      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    const browser = await chromium.launch({
+      headless: true,
+      args: ['--no-sandbox', '--disable-setuid-sandbox'], 
     });
+
     const page = await browser.newPage();
 
     await page.setContent(html, { waitUntil: 'load' });
-    const pdfBuffer = await page.pdf({ format: 'A4', printBackground: true });
+
+    const pdfBuffer = await page.pdf({
+      format: 'A4',
+      printBackground: true,
+    });
 
     await browser.close();
-    
+
     return pdfBuffer;
   }
 }
